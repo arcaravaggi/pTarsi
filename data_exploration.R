6b7f4ce15699184a4a405d0c46be23c4b48c0093
arcaravaggi <ar.caravaggi@gmail.com>
2018-10-17 14:46:15 +0100

    Update R code

--- data_exploration.R
+++ data_exploration.R
@@ -14,14 +14,30 @@ dat.1 <- pdat %>%
             mean_max = mean(max_t), sd_max = sd(max_t),
             r = cor(min_t, max_t))
 
-pdat %>% filter(code == "GRETI") %>% slice(which.max(max_t)) # Removing GRETI outlier identified a-priori
+pdat %>% filter(code == "GRETI") %>% slice(which.max(max_t)) # Removing GRETI outliers identified a-priori
+pdat %>% filter(code == "GRETI") %>% slice(which.min(max_t))
 pdat <- pdat[!(pdat$code == "GRETI" & pdat$ring_no == "Y638208"),]
+pdat <- pdat[!(pdat$code == "GRETI" & pdat$ring_no == "AFB9885"),]
 
-pdat %>% filter(code %in% c("BLABI", "BLUTI", "CHAFF", "GRETI", "PIEFL", "ROBIN")) %>% # Plot focal species
+pdat %>% 
+  filter(code %in% c("BLABI", "BLUTI", "CHAFF", "GRETI", "PIEFL", "ROBIN")) %>% # Plot focal species
+  group_by(code) %>% # manipulating the code to get counts per species for facet headers
+  mutate(code_count = n()) %>%
+  mutate(cor_coef = round(cor(min_t, max_t),2)) %>% # Add correlation to head
+  ungroup() %>%
+  mutate(code_updated = paste0(code, "; n=", code_count, "; R=", cor_coef)) %>%
   ggplot(., aes(min_t, max_t, colour=factor(code))) + 
   stat_smooth(method=lm, fullrange=FALSE) + 
   geom_point() +
-  facet_wrap( ~ code,  ncol=2, scales = "free") 
+  facet_wrap( ~ code_updated,  ncol=2, scales = "free") 
+
+mtcars %>% 
+  group_by(carb) %>%
+  mutate(carb_count = n()) %>%
+  ungroup() %>%
+  mutate(carb_updated = paste0(carb, "; n=", carb_count)) %>%
+  ggplot(aes(x = cyl)) + geom_bar()+
+  facet_wrap(~carb_updated)
 
 #### As above, but also split by sex
 dat.2 <- pdat %>% 
